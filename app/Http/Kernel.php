2fe3692c3994ea78f8665f489a281c7991896352
Roy Ho <kwanyin2000@gmail.com>
2021-02-25 03:59:40 +0800

    fix cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,7 +40,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class, // is not required for token auth
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
