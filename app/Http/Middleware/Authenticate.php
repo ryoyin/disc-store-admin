4f71fee0dd0a4e5affb085a8ae279bbce5c094c1
Roy Ho <kwanyin2000@gmail.com>
2021-02-25 01:51:02 +0800

    add 401 response for API request

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,8 +14,11 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        if (! $request->expectsJson()) {
-            return route('login');
+        // set header - Accept: application/json to receive correct error message for api request
+        if ($request->expectsJson()) {
+            return response()->json(['error' => 'Unauthenticated.'], 401);
         }
+        
+        return route('index');
     }
 }
